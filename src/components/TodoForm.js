03a75a19ca7be5f8a89e2537f91a3d867954a01e
Takshit Saini <94343242+takshittt@users.noreply.github.com>
2024-12-28 03:41:43 +0530

    Initial commit

---
+++ src/components/TodoForm.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./TodoForm.css";
+
+function TodoForm({ addTodo }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && description) {
+      addTodo({ title, description, done: false });
+      setTitle("");
+      setDescription("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <textarea
+        placeholder="Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        required
+      ></textarea>
+      <button type="submit">Add Task</button>
+    </form>
+  );
+}
+
+export default TodoForm;
